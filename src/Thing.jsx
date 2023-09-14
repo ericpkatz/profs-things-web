8b5c5094eddb10b2a55324bb8e5c2af6ea9b8977
ericpkatz <ericpkatz@gmail.com>
2023-09-14 11:04:41 -0400

    added error handling

--- src/Thing.jsx
+++ src/Thing.jsx
@@ -7,11 +7,13 @@ const Thing = ({ things, destroyThing, updateThing })=> {
   const id = params.id*1;
   const thing = things.find(thing => thing.id === id);
   const [name, setName] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(()=> {
     const thing = things.find(thing => thing.id === id);
     if(thing){
       setName(thing.name);
+      setError('');
     }
   }, [things, id]);
 
@@ -19,16 +21,24 @@ const Thing = ({ things, destroyThing, updateThing })=> {
     return null;
   }
 
-  const save = (ev)=> {
+  const save = async(ev)=> {
     ev.preventDefault();
     const thing = { id, name };
-    updateThing(thing);
+    try {
+      await updateThing(thing);
+    }
+    catch(ex){
+      setError(ex.response.data.error);
+    }
   };
 
   return (
     <div>
       <h1>Detail for { thing.name }</h1>
       <form onSubmit={ save }>
+        {
+          error ? <pre className='error'>{ JSON.stringify(error, null, 2) }</pre>: ''
+        }
         <input value={ name } onChange={ ev => setName(ev.target.value)}/>
         <button disabled={ thing.name === name }>Update</button>
       </form>
